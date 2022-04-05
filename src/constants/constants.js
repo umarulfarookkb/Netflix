49e52663c2d0743557690c3e2eb46d5ba6b5a371
umarulfarookkb <57516700+umarulfarookkb@users.noreply.github.com>
2022-04-05 23:43:12 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = '8f8a351a0e760b63b86194c9d710d44d';
+export const API_KEY = 'your api key(Generate from tmdb)';
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
